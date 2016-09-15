c992e10b5dc54e0cde5ac820852a6ba38d1ae7c3
Georgiana Ginghina <gginghina@scottlogic.co.uk>
2016-09-15 14:27:08 +0100

    config done

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-jscs");
+
+    var files = ["Gruntfile.js", "server.js", "server/**/*.js", "test/**/*.js", "client/**/*.js"];
+    var artifactsLocation = "build_artifacts";
+
+    grunt.initConfig({
+        jshint: {
+            all: files,
+            options: {
+                jshintrc: true
+            }
+        },
+        jscs: {
+            all: files,
+            options: {
+                config: ".jscsrc",
+                fix: true
+            }
+        }
+    });
+
+    grunt.registerTask("check", ["jshint", "jscs"]);
+    grunt.registerTask("test", ["check"]);
+    grunt.registerTask("default", "test");
+};
