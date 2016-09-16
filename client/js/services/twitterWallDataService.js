21a6d7e0500d36bda5d18d0dc70f45e196ed6b3a
Stephen Tozer <stozer@scottlogic.co.uk>
2016-09-16 15:37:21 +0100

    Add blank data service to app

---
+++ client/js/services/twitterWallDataService.js
@@ -0,0 +1,13 @@
+(function() {
+    angular
+        .module("TwitterWallApp")
+        .factory("twitterWallDataService", twitterWallDataService);
+
+    twitterWallDataService.$inject = ["$http"];
+
+    function twitterWallDataService($http) {
+        return {
+        };
+    }
+
+})();
