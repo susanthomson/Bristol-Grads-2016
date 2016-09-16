bcfaea282229c7e4ec16890c66ea0a810162f32b
Susan Thomson <sthomson@scottlogic.co.uk>
2016-09-16 15:17:55 +0100

    first cut at oauth admin dash

--- server/server.js
+++ server/server.js
@@ -1,10 +1,53 @@
 var express = require("express");
 var Twitter = require("twitter");
+var cookieParser = require("cookie-parser");
 
-module.exports = function(port) {
+
+module.exports = function(port, googleAuthoriser) {
     var app = express();
 
+    var adminToken;
+
     app.use(express.static("client"));
+    app.use(cookieParser());
+
+    app.get("/oauth", function(req, res) {
+        googleAuthoriser.authorise(req, function(err, token) {
+            if (!err) {
+                console.log("success");
+                adminToken = token;
+                res.cookie("sessionToken", token);
+                res.header("Location", "/dash.html");
+                res.sendStatus(302);
+            }
+            else {
+                console.log(err);
+                res.sendStatus(400);
+            }
+        });
+    });
+
+    app.get("/api/oauth/uri", function(req, res) {
+        res.json({
+            uri: googleAuthoriser.oAuthUri
+        });
+    });
+
+    app.use("/admin", function(req, res, next) {
+        if (req.cookies.sessionToken) {
+            if (req.cookies.sessionToken === adminToken) {
+                next();
+            } else {
+                res.sendStatus(401);
+            }
+        } else {
+            res.sendStatus(401);
+        }
+    });
+
+    app.get("/admin", function(req, res) {
+        res.sendStatus(200);
+    });
 
     var client = new Twitter({
         consumer_key: process.env.TWITTER_CONSUMER_KEY,
