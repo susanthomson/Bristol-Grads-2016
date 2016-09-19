d0c475ee228f77f1d6b887ca5606553970281e18
Georgiana Ginghina <gginghina@scottlogic.co.uk>
2016-09-19 15:59:51 +0100

    fixed lint and deleted unused reference

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var server = require("./server/server");
 var oAuthGoogle = require("./server/oauth-google");
-var moment = require('moment');
-moment().format();
 
 var port = process.env.PORT || 8080;
 var oauthClientId = "627385202945-oqedl0onib41h39quc15pufqgqp8j8cu.apps.googleusercontent.com";
