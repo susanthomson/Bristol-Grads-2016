77d7e5591db81f7d31c6da32f59b41956186983c
Stephen Tozer <stozer@scottlogic.co.uk>
2016-09-19 17:09:40 +0100

    Split Twitter wall data service tests by function

--- spec/client/services/twitterWallDataService.spec.js
+++ spec/client/services/twitterWallDataService.spec.js
@@ -14,14 +14,8 @@ describe("twitterWallDataService", function () {
             user: "Test user 2"
         },
     ];
-    var testTweetResponse = {
-        data: testTweets,
-    };
 
     var testMotd = "MOTD";
-    var testMotdResponse = {
-        data: "MOTD",
-    };
 
     beforeEach(module("TwitterWallApp"));
 
@@ -37,57 +31,61 @@ describe("twitterWallDataService", function () {
             .respond(testMotd);
     }));
 
-    it("returns a promise which resolves with a list of the tweet objects sent by the server when getTweets is called",
-        function (done) {
-            var failed = jasmine.createSpy("failed");
-            $httpMock.expectGET("/api/tweets");
-            twitterWallDataService.getTweets().catch(failed).then(function(result) {
-                expect(failed.calls.any()).toEqual(false);
-                expect(result).toEqual(testTweets);
-                done();
-            });
-            $httpMock.flush();
-        }
-    );
+    describe("getTweets", function() {
+        it("returns a promise which resolves with a list of the tweet objects sent by the server when getTweets is called",
+            function (done) {
+                var failed = jasmine.createSpy("failed");
+                $httpMock.expectGET("/api/tweets");
+                twitterWallDataService.getTweets().catch(failed).then(function(result) {
+                    expect(failed.calls.any()).toEqual(false);
+                    expect(result).toEqual(testTweets);
+                    done();
+                });
+                $httpMock.flush();
+            }
+        );
 
-    it("returns a promise which rejects when getTweets is called and the server returns an error code",
-        function (done) {
-            var failed = jasmine.createSpy("failed");
-            $httpMock.expectGET("/api/tweets").respond(500, "");
-            twitterWallDataService.getTweets().catch(failed).then(function(result) {
-                expect(failed.calls.any()).toEqual(true);
-                expect(failed.calls.argsFor(0)[0].status).toEqual(500);
-                done();
-            });
-            $httpMock.flush();
-        }
-    );
+        it("returns a promise which rejects when getTweets is called and the server returns an error code",
+            function (done) {
+                var failed = jasmine.createSpy("failed");
+                $httpMock.expectGET("/api/tweets").respond(500, "");
+                twitterWallDataService.getTweets().catch(failed).then(function(result) {
+                    expect(failed.calls.any()).toEqual(true);
+                    expect(failed.calls.argsFor(0)[0].status).toEqual(500);
+                    done();
+                });
+                $httpMock.flush();
+            }
+        );
+    });
 
-    it("returns a promise which resolves with the MOTD sent by the server when getMotd is called",
-        function (done) {
-            var failed = jasmine.createSpy("failed");
-            $httpMock.expectGET("/api/motd");
-            twitterWallDataService.getMotd().catch(failed).then(function(result) {
-                expect(failed.calls.any()).toEqual(false);
-                expect(result).toEqual(testMotd);
-                done();
-            });
-            $httpMock.flush();
-        }
-    );
+    describe("getMotd", function() {
+        it("returns a promise which resolves with the MOTD sent by the server when getMotd is called",
+            function (done) {
+                var failed = jasmine.createSpy("failed");
+                $httpMock.expectGET("/api/motd");
+                twitterWallDataService.getMotd().catch(failed).then(function(result) {
+                    expect(failed.calls.any()).toEqual(false);
+                    expect(result).toEqual(testMotd);
+                    done();
+                });
+                $httpMock.flush();
+            }
+        );
 
-    it("returns a promise which rejects when getMotd is called and the server returns an error code",
-        function (done) {
-            var failed = jasmine.createSpy("failed");
-            $httpMock.expectGET("/api/motd").respond(500, "");
-            twitterWallDataService.getMotd().catch(failed).then(function(result) {
-                expect(failed.calls.any()).toEqual(true);
-                expect(failed.calls.argsFor(0)[0].status).toEqual(500);
-                done();
-            });
-            $httpMock.flush();
-        }
-    );
+        it("returns a promise which rejects when getMotd is called and the server returns an error code",
+            function (done) {
+                var failed = jasmine.createSpy("failed");
+                $httpMock.expectGET("/api/motd").respond(500, "");
+                twitterWallDataService.getMotd().catch(failed).then(function(result) {
+                    expect(failed.calls.any()).toEqual(true);
+                    expect(failed.calls.argsFor(0)[0].status).toEqual(500);
+                    done();
+                });
+                $httpMock.flush();
+            }
+        );
+    });
 
 });
 
