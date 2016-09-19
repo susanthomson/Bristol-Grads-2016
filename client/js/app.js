ab9ad6e58a1e794226d0b5f5ff31c859da2e9ecc
Georgiana Ginghina <gginghina@scottlogic.co.uk>
2016-09-19 08:12:28 +0100

    data binding finished

--- client/js/app.js
+++ client/js/app.js
@@ -1,5 +1,4 @@
 (function() {
-    angular
-        .module("TwitterWallApp", []);
+    angular.module("TwitterWallApp", []);
 })();
 
--- client/js/controllers/MainController.js
+++ client/js/controllers/MainController.js
@@ -1,4 +1,9 @@
-angular.app("TwitterWallApp").controller("MainController", function($scope) {
-    $scope.tweets = ["hello1", "hello2"];
+angular.module("TwitterWallApp").controller("MainController", function($scope, $http) {
+    $scope.tweets = [];
 
+    $http.get("api/test").then(function(response) {
+    	$scope.tweets = response.data;
+    });
 });
+
+
