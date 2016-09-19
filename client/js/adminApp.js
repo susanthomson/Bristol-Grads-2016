14245563d7a4515a07daa27c5031917baafa2491
Stephen Tozer <stozer@scottlogic.co.uk>
2016-09-19 11:12:55 +0100

    Change server admin middleware to allow for multiple admin sessions

--- client/js/adminApp.js
+++ client/js/adminApp.js
@@ -1,19 +1,4 @@
 (function() {
-
-    var app = angular.module("TwitterWall", [])
-    .controller("DashController", ["$scope", "$http",
-    function($scope, $http) {
-        $scope.loggedIn = false;
-
-        $http.get("/admin").then(function() {
-            $scope.loggedIn = true;
-        }, function() {
-            $http.get("/api/oauth/uri").then(function(result) {
-                $scope.loginUri = result.data.uri;
-            });
-        });
-
-    }]);
-
+    angular.module("TwitterWallAdminApp", []);
 })();
 
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var cookieParser = require("cookie-parser");
 module.exports = function(port, googleAuthoriser) {
     var app = express();
 
-    var adminToken;
+    var adminSessions = {};
 
     app.use(express.static("client"));
     app.use(cookieParser());
@@ -14,7 +14,7 @@ module.exports = function(port, googleAuthoriser) {
         googleAuthoriser.authorise(req, function(err, token) {
             if (!err) {
                 console.log("success");
-                adminToken = token;
+                adminSessions[token] = true;
                 res.cookie("sessionToken", token);
                 res.header("Location", "/admin/dash.html");
                 res.sendStatus(302);
@@ -34,7 +34,7 @@ module.exports = function(port, googleAuthoriser) {
 
     app.use("/admin", function(req, res, next) {
         if (req.cookies.sessionToken) {
-            if (req.cookies.sessionToken === adminToken) {
+            if (adminSessions[req.cookies.sessionToken]) {
                 next();
             } else {
                 res.sendStatus(401);
