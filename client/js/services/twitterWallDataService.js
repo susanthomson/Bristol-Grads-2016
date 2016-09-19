4df4a974643532910ee7a3702891830f1390efe6
Stephen Tozer <stozer@scottlogic.co.uk>
2016-09-19 14:17:51 +0100

    Set user page to use data service

--- client/js/services/twitterWallDataService.js
+++ client/js/services/twitterWallDataService.js
@@ -8,10 +8,19 @@
     function twitterWallDataService($http) {
         return {
             getTweets: getTweets,
+            getMotd: getMotd,
         };
 
         function getTweets() {
-            return $http.get("/api/tweets");
+            return $http.get("/api/tweets").then(function(result) {
+                return result.data;
+            });
+        }
+
+        function getMotd() {
+            return $http.get("/api/motd").then(function(result) {
+                return result.data;
+            });
         }
     }
 
