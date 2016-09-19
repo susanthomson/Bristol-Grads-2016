c1f0b72560c6df0bfc62d21523e609c5e8050c5d
Georgiana Ginghina <gginghina@scottlogic.co.uk>
2016-09-19 13:44:43 +0100

    fixed small bug

--- client/js/controllers/MainController.js
+++ client/js/controllers/MainController.js
@@ -1,7 +1,7 @@
 angular.module("TwitterWallApp").controller("MainController", function($scope, $http) {
     $scope.tweets = [];
 
-    $http.get("api/tweets").then(function(response) {
+    $http.get("/api/tweets").then(function(response) {
         $scope.tweets = response.data;
     });
 });
