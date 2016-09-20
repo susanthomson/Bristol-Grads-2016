b5df114a492aeaaff07977eb841467e63f34571c
Georgiana Ginghina <gginghina@scottlogic.co.uk>
2016-09-20 10:26:20 +0100

    fixed mentions

--- client/js/controllers/MainController.js
+++ client/js/controllers/MainController.js
@@ -10,11 +10,12 @@
             $scope.tweets = tweets;
             $scope.tweets.forEach(function(tweet) {
             	if (tweet.entities.hashtags !== undefined) {
-                	tweet.text = $sce.trustAsHtml(addHashtag(tweet.text, tweet.entities.hashtags));
+                	tweet.text = addHashtag(tweet.text, tweet.entities.hashtags);
             	}
-            	if (tweet.entities.mentions !== undefined) {
-                	tweet.text = $sce.trustAsHtml(addMention(tweet.text, tweet.entities.user_mentions));
+            	if (tweet.entities.user_mentions != undefined && tweet.entities.user_mentions.length > 0) {
+                	tweet.text = addMention(tweet.text, tweet.entities.user_mentions);
             	}
+            	tweet.text = $sce.trustAsHtml(tweet.text);
             });
         });
 
@@ -28,7 +29,7 @@
 
         function addMention(str, mentions) {
         	mentions.forEach(function(mention) {
-                	var substr = mention.text;
+                	var substr = mention.screen_name;
                     str = str.split("@" + substr).join(" <b>@" + substr + "</b> ");
            });
             return str;
