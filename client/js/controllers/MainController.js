7b49f1410b7187542c756f991b53fb216987160a
Georgiana Ginghina <gginghina@scottlogic.co.uk>
2016-09-20 09:02:00 +0100

    added html formatting tags on tweets to show blue hashtags and mentions

--- client/js/controllers/MainController.js
+++ client/js/controllers/MainController.js
@@ -8,6 +8,14 @@
 
         twitterWallDataService.getTweets().then(function(tweets) {
             $scope.tweets = tweets;
+            $scope.tweets.forEach(function(tweet) {
+            	tweet.entities.hashtags.forEach(function(hashtag){
+					tweet.text = tweet.text.split("#" + hashtag.text).join("<a href=''><b>#" + hashtag + "</b></a>");
+            	});
+            	tweet.entities.user_mentions.forEach(function(mention){
+					tweet.text = tweet.text.split("@" + mention.text).join("<a href=''><b>@" + mention + "</b></a>");
+            	});
+        	});
         });
 
         twitterWallDataService.getMotd().then(function(motd) {
