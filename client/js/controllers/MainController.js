c3d80042ad3678604d3cadeac067115a89c5b684
Georgiana Ginghina <gginghina@scottlogic.co.uk>
2016-09-20 10:50:17 +0100

    now deleting the media url from the tweet text

--- client/js/controllers/MainController.js
+++ client/js/controllers/MainController.js
@@ -12,6 +12,7 @@
                 tweet.text = addHashtag(tweet.text, tweet.entities.hashtags);
                 tweet.text = addMention(tweet.text, tweet.entities.user_mentions);
                 tweet.text = addUrl(tweet.text, tweet.entities.urls);
+                if (tweet.entities.media) tweet.text = deleteMediaLink(tweet.text, tweet.entities.media);
             	tweet.text = $sce.trustAsHtml(tweet.text);
             });
         });
@@ -32,13 +33,19 @@
             return str;
         }
 
-        function addUrl(str, urls) {console.log(urls.length);
+        function addUrl(str, urls) {
         	urls.forEach(function(uri) {
                 	var substr = uri.url;
-                	
                     str = str.split(substr).join("  <b>" + uri.display_url + "</b>  ");
            });
             return str;
+        }
+
+        function deleteMediaLink(str, media) {
+        	media.forEach(function(m) {
+        		str = str.split(m.url).join("");
+        	}); 
+        	return str;
         }
 
         twitterWallDataService.getMotd().then(function(motd) {
