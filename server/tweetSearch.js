7cabe501075a5d157e729fdeef0f2a921ed5b130
Stephen Tozer <stozer@scottlogic.co.uk>
2016-09-21 11:10:10 +0100

    Added generic functions for getting updates on Twitter resources

--- server/tweetSearch.js
+++ server/tweetSearch.js
@@ -8,44 +8,54 @@ module.exports = function(client) {
             basePath: "search",
             requestsRemaining: 0,
             resetTime: 0,
+            addData: function(tweets) {
+                this.since_id = tweets.statuses.reduce(function(since, currTweet) {
+                    return since > currTweet.id ? since : currTweet.id;
+                }, this.since_id);
+                tweetStore = tweetStore.concat(tweets.statuses.sort(function(statusA, statusB) {
+                    return statusA.id - statusB.id;
+                }));
+            }
         },
         "statuses/user_timeline": {
             since_id: 0,
             basePath: "statuses",
             requestsRemaining: 0,
             resetTime: 0,
+            addData: function(tweets) {
+                this.since_id = tweets.reduce(function(since, currTweet) {
+                    return since > currTweet.id ? since : currTweet.id;
+                }, this.since_id);
+                tweetStore = tweetStore.concat(tweets.sort(function(statusA, statusB) {
+                    return statusA.id - statusB.id;
+                }));
+            }
         },
     };
 
     var searchUpdater;
     var userUpdater;
 
-    var searchUpdate = function() {
-        if (apiResources["search/tweets"].requestsRemaining > 0) {
-            getTweetsWithHashtag();
-            searchUpdater = setTimeout(searchUpdate, 5000);
+    function resourceUpdate(apiResource, updateFn, timer) {
+        if (apiResources[apiResource].requestsRemaining > 0) {
+            updateFn();
+            timer = setTimeout(function() {
+                resourceUpdate(apiResource, updateFn, timer);
+            }, 5000);
         } else {
-            searchUpdater = setTimeout(function() {
-                apiResources["search/tweets"].requestsRemaining = 1;
-                searchUpdate();
-            }, apiResources["search/tweets"].resetTime - new Date().getTime());
+            timer = setTimeout(function() {
+                apiResources[apiResource].requestsRemaining = 1;
+                resourceUpdate(apiResource, updateFn, timer);
+            }, apiResources[apiResource].resetTime - new Date().getTime());
         }
-    };
-    var userUpdate = function() {
-        if (apiResources["statuses/user_timeline"].requestsRemaining > 0) {
-            getTweetsFrom("bristech");
-            userUpdater = setTimeout(userUpdate, 5000);
-        } else {
-            userUpdater = setTimeout(function() {
-                apiResources["statuses/user_timeline"].requestsRemaining = 1;
-                userUpdate();
-            }, apiResources["statuses/user_timeline"].resetTime - new Date().getTime());
-        }
-    };
+    }
+
+    var hashtagUpdateFn = tweetResourceGetter("search/tweets", {q: hashtags.join(" OR ")});
+    var timelineUpdateFn = tweetResourceGetter("statuses/user_timeline", {screen_name: "bristech"});
 
     getApplicationRateLimits(function() {
-        searchUpdate();
-        userUpdate();
+        resourceUpdate("search/tweets", hashtagUpdateFn, searchUpdater);
+        resourceUpdate("statuses/user_timeline", timelineUpdateFn, userUpdater);
     });
 
     return {
@@ -69,46 +79,20 @@ module.exports = function(client) {
         tweetStore = value;
     }
 
-    function getTweetsWithHashtag() {
-        var query = {
-            q: hashtags.join(" OR "),
-        };
-        var last_id = apiResources["search/tweets"].since_id;
-        if (last_id > 0) {
-            query.since_id = last_id;
-        }
-        client.get("search/tweets", query, function(error, tweets, response) {
-            if (tweets) {
-                apiResources["search/tweets"].since_id = tweets.statuses.reduce(function(since, currTweet) {
-                    return since > currTweet.id ? since : currTweet.id;
-                }, apiResources["search/tweets"].since_id);
-                tweetStore = tweetStore.concat(tweets.statuses.sort(function(statusA, statusB) {
-                    return statusA.id - statusB.id;
-                }));
-                apiResources["search/tweets"].requestsRemaining = response.headers["x-rate-limit-remaining"];
-                apiResources["search/tweets"].resetTime = (Number(response.headers["x-rate-limit-reset"]) + 1) * 1000;
-            } else {
-                console.log(error);
-            }
-        });
+    function tweetResourceGetter(resource, query) {
+        return getTweetResource.bind(undefined, resource, query);
     }
 
-    function getTweetsFrom(screenName) {
-        var query = {screen_name: screenName};
-        var last_id = apiResources["statuses/user_timeline"].since_id;
+    function getTweetResource(resource, query) {
+        var last_id = apiResources[resource].since_id;
         if (last_id > 0) {
             query.since_id = last_id;
         }
-        client.get("statuses/user_timeline", query, function(error, tweets, response) {
-            if (tweets) {
-                apiResources["statuses/user_timeline"].since_id = tweets.reduce(function(since, currTweet) {
-                    return since > currTweet.id ? since : currTweet.id;
-                }, apiResources["statuses/user_timeline"].since_id);
-                tweetStore = tweetStore.concat(tweets.sort(function(statusA, statusB) {
-                    return statusA.id - statusB.id;
-                }));
-                apiResources["statuses/user_timeline"].requestsRemaining = response.headers["x-rate-limit-remaining"];
-                apiResources["statuses/user_timeline"].resetTime = (Number(response.headers["x-rate-limit-reset"]) + 1) * 1000;
+        client.get(resource, query, function(error, data, response) {
+            if (data) {
+                apiResources[resource].addData(data);
+                apiResources[resource].requestsRemaining = response.headers["x-rate-limit-remaining"];
+                apiResources[resource].resetTime = (Number(response.headers["x-rate-limit-reset"]) + 1) * 1000;
             } else {
                 console.log(error);
             }
