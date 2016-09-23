a0159b50ec0bc39a85aedd6770687d13602af143
Georgiana Ginghina <gginghina@scottlogic.co.uk>
2016-09-23 16:56:00 +0100

    added test for setting the delete flag on admin page

--- client/angular/controllers/AdminController.js
+++ client/angular/controllers/AdminController.js
@@ -67,17 +67,22 @@
                 $scope.tweets = $scope.tweets.concat(results.tweets);
                 if (results.updates.length > 0) {
                     vm.latestUpdateTime = results.updates[results.updates.length - 1].since;
-                    results.updates.forEach(function(del) {
-                        if (del.type === "tweet_status" && del.status.deleted) {
-                            $scope.tweets.forEach(function(tweet) {
-                                if (tweet.id_str === del.id) {
-                                    tweet.deleted = true;
-                                }
-                            });
+                    $scope.tweets = $scope.setDeletedFlagForDeletedTweets($scope.tweets, results.updates);
+                }
+            });
+        }
+
+        $scope.setDeletedFlagForDeletedTweets = function(tweets, updates) {
+            updates.forEach(function(del) {
+                if (del.type === "tweet_status" && del.status.deleted) {
+                    tweets.forEach(function(tweet) {
+                        if (tweet.id_str === del.id) {
+                            tweet.deleted = true;
                         }
                     });
                 }
             });
-        }
+            return tweets;
+        };
     }
 })();
