c0778a9e0b288763f7678fa577c7204992251d26
Georgiana Ginghina <gginghina@scottlogic.co.uk>
2016-09-27 12:00:32 +0100

    delete console log

--- server/server.js
+++ server/server.js
@@ -91,7 +91,6 @@ module.exports = function(port, tweetSearcher, googleAuthoriser) {
             tweetSearcher.addBlockedUser(req.body.user);
             res.sendStatus(200);
         } catch (err) {
-            console.log(err);
             res.sendStatus(404);
         }
     });
