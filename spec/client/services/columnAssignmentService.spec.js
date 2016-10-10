0b3fafe1dfb3ec5518975c15212ef8ac7263f1bc
Stephen Tozer <stozer@scottlogic.co.uk>
2016-10-10 11:52:13 +0100

    Add tests for column assignment service

--- spec/client/services/columnAssignmentService.spec.js
+++ spec/client/services/columnAssignmentService.spec.js
@@ -15,28 +15,26 @@ describe("columnAssignmentService", function() {
 
     var testColumnDataList;
     var testTweets;
+    var testAssignedColumns;
+    var testSortedColumns;
+    var testBackfilledColumns;
 
     beforeEach(function() {
-        var x = new(columnAssignmentService.ColumnData)(2, function(tweet) {
-            return tweet.pinned === true;
-        }, function(tweetA, tweetB) {
-            return tweetA.pinTime - tweetB.pinTime;
-        });
         testColumnDataList = [
             new columnAssignmentService.ColumnData(2, function(tweet) {
                 return tweet.pinned === true;
             }, function(tweetA, tweetB) {
-                return tweetA.pinTime - tweetB.pinTime;
+                return tweetB.pinTime - tweetA.pinTime;
             }),
             new columnAssignmentService.ColumnData(3, function(tweet) {
                 return tweet.wallPriority === true;
             }, function(tweetA, tweetB) {
-                return tweetA.time - tweetB.time;
+                return tweetB.time - tweetA.time;
             }),
             new columnAssignmentService.ColumnData(3, function(tweet) {
                 return true;
             }, function(tweetA, tweetB) {
-                return tweetA.time - tweetB.time;
+                return tweetB.time - tweetA.time;
             }),
         ];
 
@@ -66,13 +64,41 @@ describe("columnAssignmentService", function() {
         }, {
             time: new Date(52)
         }];
+        testAssignedColumns = [
+            [testTweets[0], testTweets[4]],
+            [testTweets[1], testTweets[5]],
+            [testTweets[2], testTweets[3], testTweets[6], testTweets[7]],
+        ];
+        testSortedColumns = [
+            [testTweets[0], testTweets[4]],
+            [testTweets[5], testTweets[1]],
+            [testTweets[7], testTweets[6], testTweets[3], testTweets[2]],
+        ];
+        testBackfilledColumns = [
+            [testTweets[0], testTweets[4]],
+            [testTweets[5], testTweets[1]],
+            [testTweets[7], testTweets[6], testTweets[3], testTweets[2]],
+        ];
     });
 
     describe("assignColumns", function() {
-        it("returns an array of arrays with length equal to the length of the input column data list", function() {
-            var columnList = columnAssignmentService.assignColumns(testTweets, testColumnDataList);
-            expect(columnList).toEqual([jasmine.any(Array), jasmine.any(Array), jasmine.any(Array)]);
+        it("returns an array of columns, each containing tweets selected by the corresponding selector and no " +
+            "selector of higher priority",
+            function() {
+                var columnList = columnAssignmentService.assignColumns(testTweets, testColumnDataList);
+                expect(columnList).toEqual(testAssignedColumns);
+            });
+    });
+
+    describe("sortColumns", function() {
+        it("returns an array of columns, sorted according to the corresponding ordering function", function() {
+            var columnList = columnAssignmentService.sortColumns(testAssignedColumns, testColumnDataList);
+            expect(columnList).toEqual(testSortedColumns);
         });
+    });
+
+    describe("backfillColumns", function() {
+        xit("returns an array of columns backfilled according to the backfill algorithm", function() {});
     });
 
 });
