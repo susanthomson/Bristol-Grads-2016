093358f65e5f205fdf6c6319a74a9a0b4ca0c49a
Ben Howard <bhoward@scottlogic.co.uk>
2016-10-12 09:51:30 +0100

    formatting

--- client/angular/controllers/MainController.js
+++ client/angular/controllers/MainController.js
@@ -97,8 +97,8 @@
                 $document.body.clientWidth;
 
             var baseColumnWidth = ($scope.screenWidth - //total screen width
-                (2 * tweetMargin * columnDataList.length)) / //remove total size of margins between columns
-            columnDataList.length; //divide remaining space between columns
+                    (2 * tweetMargin * columnDataList.length)) / //remove total size of margins between columns
+                columnDataList.length; //divide remaining space between columns
             displayColumns.forEach(function(tweetColumn, colIdx) {
                 var baseSlotHeight = (($scope.screenHeight - //the total screen height
                         (2 * tweetMargin * columnDataList[colIdx].slots) - //remove total size of margins between tweets
