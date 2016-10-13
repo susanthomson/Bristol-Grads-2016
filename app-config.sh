a9287745438ad670fcdb7891bd0b9481ec192c71
Stephen Tozer <stozer@scottlogic.co.uk>
2016-10-13 14:29:27 +0100

    Use sudo in config script

--- app-config.sh
+++ app-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Copy server config files stored in the EC2 directory to the appropriate folder
-cp /home/ec2-user/TwitterWallConfig/adminConfig.json /home/ec2-user/TwitterWallApp/server/config/adminConfig.json
-cp /home/ec2-user/TwitterWallConfig/eventConfig.json /home/ec2-user/TwitterWallApp/server/config/eventConfig.json
+sudo cp /home/ec2-user/TwitterWallConfig/adminConfig.json /home/ec2-user/TwitterWallApp/server/config/adminConfig.json
+sudo cp /home/ec2-user/TwitterWallConfig/eventConfig.json /home/ec2-user/TwitterWallApp/server/config/eventConfig.json
 
