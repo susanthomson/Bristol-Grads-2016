de5c1850f7082bc6d68fbe548e0d1da85cd00414
Stephen Tozer <stozer@scottlogic.co.uk>
2016-10-17 12:09:54 +0100

    Add library service for general tweet functions

---
+++ client/angular/services/tweetInfoService.js
@@ -0,0 +1,23 @@
+(function() {
+    angular
+        .module("TwitterWallApp")
+        .factory("tweetInfoService", tweetInfoService);
+
+    tweetInfoService.$inject = [];
+
+    function tweetInfoService() {
+        return {
+            tweetHasImage: tweetHasImage,
+        };
+
+        function tweetHasImage(tweet, showAllImages) {
+            try {
+                return tweet.entities.media[0].media_url_https && (!tweet.hide_image || showAllImages);
+            } catch(err) {
+                return false;
+            }
+        };
+    }
+
+})();
+
--- client/main.js
+++ client/main.js
@@ -5,6 +5,7 @@ require("./angular/controllers/MainController.js");
 require("./angular/services/adminDashDataService.js");
 require("./angular/services/tweetTextManipulationService.js");
 require("./angular/services/twitterWallDataService.js");
+require("./angular/services/tweetInfoService.js");
 require("./angular/services/columnAssignmentService.js");
 require("./angular/filters/tweetSearchFilter.js");
 
