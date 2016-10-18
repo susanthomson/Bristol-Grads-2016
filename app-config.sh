a968a9a12fc799a2286d4b769a1b3d2ca2a2c6d9
Stephen Tozer <stozer@scottlogic.co.uk>
2016-10-18 09:33:33 +0100

    Access working directory and disown node proc

--- app-config.sh
+++ app-config.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+set -e
+
 sudo yum -y update
 
 echo "Installing nodejs to yum..."
 curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
 sudo yum -y install nodejs
+
+cd /home/ec2-user/TwitterWallApp
 
 echo "Installing packages..."
 sudo npm install
@@ -14,9 +18,13 @@ sudo env NODE_ENV=production grunt build
 
 # Copy server config files stored in the EC2 directory to the appropriate folder
 echo "Fetching configuration files..."
-sudo mkdir /home/ec2-user/TwitterWallApp/server/config
-sudo cp /home/ec2-user/TwitterWallConfig/adminConfig.json /home/ec2-user/TwitterWallApp/server/config/adminConfig.json
-sudo cp /home/ec2-user/TwitterWallConfig/eventConfig.json /home/ec2-user/TwitterWallApp/server/config/eventConfig.json
+sudo mkdir -p /home/ec2-user/TwitterWallApp/server/config
+sudo cp /home/ec2-user/TwitterWallConfig/adminConfig.json server/config/adminConfig.json
+sudo cp /home/ec2-user/TwitterWallConfig/eventConfig.json server/config/eventConfig.json
+
+echo "Creating log folder..."
+sudo rm -r logs
+sudo mkdir logs
 
 echo "Install complete."
 
