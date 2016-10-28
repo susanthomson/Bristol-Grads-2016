ed58a65eb308fd810a0354742fa87ab0bec4f08b
Susan Thomson <sthomson@scottlogic.co.uk>
2016-10-28 13:59:44 +0100

    added twemojis

--- client/angular/services/tweetTextManipulationService.js
+++ client/angular/services/tweetTextManipulationService.js
@@ -28,6 +28,8 @@
 
             displayText = displayText.replace(/https?\:\/\/(www.)?/i, "");
 
+            displayText = twemoji.parse(displayText);
+
             return displayText;
         }
 
