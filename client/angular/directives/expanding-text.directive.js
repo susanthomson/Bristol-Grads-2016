6ea389aff58ea2bdd9ff9a070e5367f87fa5d214
Stephen Tozer <stozer@scottlogic.co.uk>
2016-10-31 14:18:08 +0000

    Fix style errors

--- client/angular/directives/expanding-text.directive.js
+++ client/angular/directives/expanding-text.directive.js
@@ -6,8 +6,7 @@
     function expandingText($interval) {
         return {
             restrict: "A",
-            scope: {
-            },
+            scope: {},
             link: function(scope, element, attrs) {
                 var maxHeight;
                 var fontSize = 6;
@@ -15,7 +14,7 @@
                 var transitionInterval = 100;
                 var finishedResizing = false;
                 var elapsedTime = 0;
-                var padding = Number(element.css("padding-left").replace("px","")) + Number(element.css("padding-right").replace("px",""));
+                var padding = Number(element.css("padding-left").replace("px", "")) + Number(element.css("padding-right").replace("px", ""));
                 element.css("font-size", fontSize + "px");
                 var heightTimer = $interval(function() {
                     fontSize = fontSize + 3;
@@ -28,19 +27,20 @@
                         var maxWidth = contentWidth - imageWidth - padding;
                         element.css("max-width", maxWidth);
                         element.css("font-size", fontSize + "px");
-                        while(element.height() > maxHeight || element[0].scrollWidth > maxWidth) {
+                        while (element.height() > maxHeight || element[0].scrollWidth > maxWidth) {
                             fontSize -= 1;
                             element.css("font-size", fontSize + "px");
                         }
                     }
                     elapsedTime += transitionInterval;
-                    if(elapsedTime >= transitionTime) {
+                    if (elapsedTime >= transitionTime) {
                         cancelInterval();
-                    };
+                    }
                 }, transitionInterval);
                 element.on("$destroy", function() {
                     cancelInterval();
                 });
+
                 function cancelInterval() {
                     element.css("max-width", "1000px");
                     $interval.cancel(heightTimer);
@@ -49,4 +49,3 @@
         };
     }
 })();
-
